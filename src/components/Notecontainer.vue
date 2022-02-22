d9f4cba8ebcdced2c63f880e355a90549df7435e
YOUR NAME <jhanov@gmail.com>
2022-02-21 23:07:02 -0600

    add toolbar component

---
+++ src/components/Notecontainer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="note-container">
+    <Noteselectors />
+      <div class="note-editor">
+        <p class="note-editor-info">Timestamp here...</p>
+        <textarea class="note-editor-input">
+          First note...
+
+          Note text here...
+        </textarea>
+      </div>
+    </div>
+</template>
+
+<script>
+import Noteselectors from "./Noteselectors.vue";
+export default {
+  components: {
+    Noteselectors,
+  },
+};
+</script> 
